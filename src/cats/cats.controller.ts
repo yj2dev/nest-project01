48315236e6d144c392908482849828b3bbacf3b4
adx <nogo22@naver.com>
2022-01-14 16:34:43 +0900

    apply Exception filter

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Delete, Get, Patch, Post, Put } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  HttpException,
+  Patch,
+  Post,
+  Put,
+  UseFilters,
+} from '@nestjs/common';
 import { CatsService } from './cats.service';
+import { HttpExceptionFilter } from '../http-exception.filter';
 
 @Controller('cats')
+@UseFilters(HttpExceptionFilter)
 export class CatsController {
   constructor(private readonly catsService: CatsService) {}
 
   @Get()
   getAllCat() {
+    throw new HttpException('api is broken', 401);
     return 'getAllCat';
   }
 
